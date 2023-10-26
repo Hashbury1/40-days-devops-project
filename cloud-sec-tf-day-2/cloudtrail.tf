707d47ed2423169591896ad3c8dc02b5b739d32e
hashbury Env <efefiomarchibong@gmail.com>
2023-10-27 00:03:41 +0100

    Update cloudtrail.tf

--- cloud-sec-tf-day-2/cloudtrail.tf
+++ cloud-sec-tf-day-2/cloudtrail.tf
@@ -1,10 +1,11 @@
-resource "aws_cloudtrail" "example" {
-  name                         = "example-cloudtrail"
-  s3_bucket_name               = "your-s3-bucket-name"
-  include_global_service_events = true  # Set to true to include global services
+resource "aws_cloudtrail" "terra" {
+  name                          = "terra-cloudtrail"
+  s3_bucket_name                = "backend-terra-hash"
+  s3_key_prefix                 = "cloudtrail"
+  include_global_service_events = true # Set to true to include global services
 
   event_selector {
-    read_write_type = "All"  # Options: All, Read-only, Write-only
+    read_write_type = "All" # Options: All, Read-only, Write-only
   }
 
   # Add other event selectors as needed
