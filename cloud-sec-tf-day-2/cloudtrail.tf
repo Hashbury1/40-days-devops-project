54d9480212f7410deaf9740474dd2767d3dd5888
hashbury Env <efefiomarchibong@gmail.com>
2023-10-27 06:54:41 +0100

    Update cloudtrail.tf

--- cloud-sec-tf-day-2/cloudtrail.tf
+++ cloud-sec-tf-day-2/cloudtrail.tf
@@ -1,7 +1,6 @@
 resource "aws_cloudtrail" "terra" {
   name                          = "terra-cloudtrail"
   s3_bucket_name                = "backend-terra-hash"
-  s3_key_prefix                 = "cloudtrail"
   include_global_service_events = true # Set to true to include global services
 
   event_selector {
