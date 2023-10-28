b85a919977a02c7c4fb4d95c2fcf7de921c68846
hashbury Env <efefiomarchibong@gmail.com>
2023-10-28 21:04:36 +0100

    Update main.tf

--- cloud-sec-tf-day-2/main.tf
+++ cloud-sec-tf-day-2/main.tf
@@ -62,6 +62,8 @@ resource "aws_cloudwatch_log_group" "cloudwatch" {
   name = "/flow-logs" # The name of your log group
 }
 
+
+# create config recorder
 resource "aws_config_configuration_recorder" "config_recorder" {
   name     = "config-recorder"
   role_arn = "arn:aws:iam::152918265083:role/config-role"
