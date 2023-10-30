2a23ffe39a0474a5f29c5ca37a9e4dd18a18eb7e
hashbury Env <efefiomarchibong@gmail.com>
2023-10-30 08:54:31 +0100

    Update secret_manager.tf

--- cloud-sec-tf-day-2/secret_manager.tf
+++ cloud-sec-tf-day-2/secret_manager.tf
@@ -7,22 +7,22 @@ resource "aws_secretsmanager_secret" "my_secret" {
 resource "aws_secretsmanager_secret_version" "my_secret_version" {
   secret_id = aws_secretsmanager_secret.my_secret.id
   secret_string = jsonencode({
-    username = "admin",
-    password = "mypassword1234"
+    "username" : "user",
+    "password" : "mypassword"
   })
 }
 
 
 # Create RDS for your sensitive aws resources
 resource "aws_db_instance" "rds" {
-  name              = "for secret"
-  allocated_storage = 20
-  storage_type      = "gp2"
-  engine            = "mysql"
-  engine_version    = "5.7"
-  instance_class    = "db.t2.micro"
-  username          = aws_secretsmanager_secret_version.my_secret_version.secret_string["admin"]
-  password          = aws_secretsmanager_secret_version.my_secret_version.secret_string["mypassword1234"]
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  db_name                 = "Testrds003"
+  username             = "myuser"
+  password             = "mypassword1234"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
-
-
