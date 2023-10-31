3cfa34befcc2c3958d6932baa7f88a91910c709b
hashbury Env <efefiomarchibong@gmail.com>
2023-10-31 15:18:19 +0100

    update Day 2 TF resources

---
+++ Day2-cloud-sec-tf/cloudtrail.tf
@@ -0,0 +1,17 @@
+resource "aws_cloudtrail" "terra" {
+  name                          = "terra-cloudtrail"
+  s3_bucket_name                = "backend-terra-hash"
+  include_global_service_events = true # Set to true to include global services
+
+  event_selector {
+    read_write_type = "All" # Options: All, Read-only, Write-only
+  }
+
+  # Add other event selectors as needed
+
+  # Optionally, define tags for your CloudTrail
+  tags = {
+    Environment = "Production"
+    Application = "MyApp"
+  }
+}
---
+++ Day2-cloud-sec-tf/import_security_group.tf
@@ -0,0 +1,3 @@
+resource "aws_security_group" "allow_sg" {
+  # Specify the attributes you want to import here
+}
